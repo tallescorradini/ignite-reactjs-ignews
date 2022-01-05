167dfe6357dc8fd4cab08e19ade055f4a693c146
Talles Corradini <tallescorradini@gmail.com>
2022-01-05 20:55:40 -0300

    feat: add hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,34 @@
 import Head from "next/head";
 
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
-        <title> Inicio | Ig.news</title>
+        <title> Home | Ig.news</title>
       </Head>
       <header>
         <h1>Hello world</h1>
       </header>
-    </div>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90</span>
+          </p>
+        </section>
+
+        <img
+          src="/images/avatar.svg"
+          alt="Girl sitting behind a desk coding on a laptop"
+        />
+      </main>
+    </>
   );
 }
