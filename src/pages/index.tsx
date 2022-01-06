da50d2d1452c613863501d701fec9af9b2adaa96
Talles Corradini <tallescorradini@gmail.com>
2022-01-05 21:02:46 -0300

    feat: add hero subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from "./home.module.scss";
 
@@ -22,6 +23,7 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.90</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <img
